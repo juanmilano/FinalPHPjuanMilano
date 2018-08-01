4d859bfb61e80a93f33197c9831eebe6aa54fe1b
Juan Milano <getoo.hh@gmail.com>
2018-08-01 17:26:19 -0300

    final con json

--- SECCIONES/gracias.php
+++ SECCIONES/gracias.php
@@ -1,4 +1,14 @@
+<?php
 
+if(file_exists("formulario.json")){
+    
+    $datos = file_get_contents("formulario.json");
+    
+    $formulario = json_decode($datos,true);
+   }
+
+?>
+    
 <section class="primary" id="gracias">
             <div class="row">
                             <div class="form-group col-xs-12">
@@ -10,35 +20,35 @@
                     <form name="sentMessage" id="contactForm" form action="" method="">
                         <div class="row control-group">
                             <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <?php echo "<label>Su Nombre y Apellido: </label></br>".$_GET['quenombre']."<br>";?> 
+                                <?php echo "<label>Su Nombre y Apellido: </label></br>".$formulario['name']."<br>";?> 
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div class="row control-group">
                             <div class="form-group col-xs-12 floating-label-form-group controls">
-                              <?php echo "<label>Su Email: </label></br>".$_GET['queemail']."<br>";?> 
+                              <?php echo "<label>Su Email: </label></br>".$formulario['email']."<br>";?> 
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div class="row control-group">
                             <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <?php echo "<label>Su Telélefono: </label></br>".$_GET['quephone']."<br>";?>  
+                                <?php echo "<label>Su Telélefono: </label></br>".$formulario['phone']."<br>";?>  
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div class="row control-group">
                             <div class="form-group col-xs-12 floating-label-form-group controls">
-                            <?php echo "<label>Su Nombre: </label></br>".$_GET['quemensaje']."<br>";?> 
+                            <?php echo "<label>Mensaje: </label></br>".$formulario['message']."<br>";?> 
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div>
                     <span class="help-block"><p id="characterLeft" class="help-block ">¿Sobre qué tipo de eventos queres recibir información?</p></span>
                     <?php    
-                            if(empty($_GET['quefiesta'])){
+                            if(empty($formulario['fiesta'])){
                             echo "Aun no decidió el tipo de evento";
                     }else {   
-                        $fiestas = unserialize($_GET['quefiesta']);
+                        $fiestas = unserialize($formulario['fiesta']);
                         echo "<ul>";
                             foreach($fiestas as $valor){
                                 echo "<li>$valor</li>";            
--- funciones.php
+++ funciones.php
@@ -1,14 +1,31 @@
 <?php
 function compruebaForm($campo){
+
 $name = trim($campo['name']);
 $email = trim($campo['email']);
 $phone = trim($campo['phone']);
 $message = htmlentities(nl2br(trim($_POST['message'])));
 $fiesta = isset($campo['fiesta']) ? serialize($campo['fiesta']) : '';
 
-$datosRecibidos = "&quenombre=$name&queemail=$email&quephone=$phone&quefiesta=$fiesta&quemensaje=$message";
 
-if (empty($campo['name']) || empty($campo['email']) || empty($campo['phone']) || empty($campo['message'])){
+$formulario = [];
+$formulario  = [
+	"name" => $name,
+	"email" => $email,
+	"phone" => $phone,
+	"message" => $message,
+	"fiesta" => $fiesta
+
+];
+
+header("Content-Type: application/json; charset=UTF-8");
+$formularioJSON = json_encode($formulario);
+
+file_put_contents("formulario.json",$formularioJSON);
+
+$datosRecibidos = "&quenombre=$formulario[name]&queemail=$formulario[email]&quephone=$formulario[phone]&quefiesta=$formulario[fiesta]&quemensaje=$formulario[message]";
+
+if (empty($formulario['name']) || empty($formulario['email']) || empty($formulario['phone']) || empty($formulario['message'])){
     header("Location:index.php?seccion=errorform&datosRecibidos=$datosRecibidos");
     die;
 }else{
